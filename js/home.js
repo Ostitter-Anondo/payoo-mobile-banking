7ecd97e4002b2738c46a47e3b7875f6316c47ca3
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-09-19 04:09:42 +0600

    login pseudo-backend done

---
+++ js/home.js
@@ -0,0 +1,13 @@
+document.getElementById('addmoney-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  let currentBal = Number(document.getElementById('cashmoney').textContent);
+  const userPin = document.getElementById('user-pin').value;
+  const toAdd = Number(document.getElementById('cash-amount').value);
+  if (userPin === '1474'){
+    currentBal += toAdd;
+    document.getElementById('cashmoney').textContent = currentBal;
+  }
+  else {
+    document.getElementById('error').innerHTML = "<span class='text-red-600'>wrong phone number or pin</span>";
+  }
+})
