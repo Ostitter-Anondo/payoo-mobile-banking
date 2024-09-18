361d6d12e0e507b22d2123ebbb505afe3f8b1067
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-09-19 02:41:37 +0600

    login pseudo-backend done

--- js/login.js
+++ js/login.js
@@ -2,6 +2,13 @@ const loginBtn = document.getElementById('login-btn');
 loginBtn.addEventListener('click', function(event){
   event.preventDefault();
   const phoneNo = document.getElementById('phone-number').value;
-  // const pinNo = document.getElementById('user-pin').value;
-  console.log(phoneNo);
+  const pinNo = document.getElementById('user-pin').value;
+  
+  // not ideal but for now we'll go with this
+  if (phoneNo === '01886318799' && pinNo === `1474`){
+    window.location.href = 'home.html'
+  }
+  else {
+    document.getElementById('error').innerHTML = "<span class='text-red-600'>wrong phone number or pin</span>";
+  }
 })
